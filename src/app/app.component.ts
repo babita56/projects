6e5ae12aaa13a08dea6e99f6119ad72581faba21
Babita Chaturvedi <babita.chaturvedi@seceon.com>
2024-02-16 12:53:23 +0530

    commit for the html changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table';
+
+   firstname ="babita"
+   lastname = "chaturvedi"
+
+
+   // array of object 
+   memberData  = [
+
+
+    
+    {name:"babita" ,email :"baita@gmail.com"},
+    {name:"kuttu" ,email :"kuttu@gmail.com"},
+    {name:"neelam" ,email :"neelam@gmail.com"},
+    {name:"shivanand" ,email :"shivanand@gmail.com"},
+    {name:"swaraj" ,email :"swaraj@gmail.com"},
+    {name:"xyc" ,email :"xyc@gmail.com"}
+
+     
+   ]
+
+
+
+
+
 }
